66d0cc5164560be60030b4104b9e83c05675cf85
Jordan Holtkamp <jordanholtkamp@gmail.com>
2022-09-07 01:54:30 +0700

    update testing

--- spec/address_service_spec.rb
+++ spec/address_service_spec.rb
@@ -3,6 +3,7 @@ require './services/address_service'
 
 describe AddressService, type: :model do
     it 'makes request to Smarty API', :vcr do
+        json_response = JSON.parse(File.read("./fixtures/api_response.json"))
 
         ENV['ADDRESS_API_KEY'] = "test_key"
 
@@ -12,12 +13,18 @@ describe AddressService, type: :model do
             "zipcode" => "43201"
         }
 
-        puts "API_KEY = #{ENV['ADDRESS_API_KEY']}"
-
-        response = AddressService.new(address_hash).parsed_response
+        response = AddressService.new.parsed_response(json_response)
         expect(response[0]).to have_key("delivery_line_1")
         expect(response[0]).to have_key("components")
         expect(response[0]["components"]).to have_key("city_name")
         expect(response[0]["components"]).to have_key("zipcode")
+    end
+
+    it 'handles no csv file argument' do
+        address_service = AddressService.new
+
+        error_handle = address_service.validate_input(nil)
+        require 'pry'; binding.pry
+        expect(error_handle).to include("LOG.ERROR")
     end
 end
